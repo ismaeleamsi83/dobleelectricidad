414b31c0139719c4805ca6d77e8aa3e648448050
ismael <ismaelreyplata@gmail.com>
2024-10-08 11:28:40 +0200

    consultar datos personales, tabla creada con orden y paginacion

--- src/app/components/invoicequery/invoicequery.component.ts
+++ src/app/components/invoicequery/invoicequery.component.ts
@@ -39,12 +39,7 @@ export class InvoicequeryComponent implements OnInit, AfterViewInit {
     this.dataSource.paginator = this.paginator; 
   }
 
-  /** Announce the change in sort state for assistive technology. */
   announceSortChange(sortState: Sort) {
-    // This example uses English messages. If your application supports
-    // multiple language, you would internationalize these strings.
-    // Furthermore, you can customize the message to add additional
-    // details about the values being sorted.
     if (sortState.direction) {
       this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
     } else {
