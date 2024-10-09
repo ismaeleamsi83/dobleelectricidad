dafe0a4923b04c915c8d637d84bf1db69254b907
ismael <ismaelreyplata@gmail.com>
2024-10-09 23:29:14 +0200

    multiidioma ok

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,11 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
-
+import { LOCALE_ID } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideAnimationsAsync(),
-              provideAnimationsAsync(), provideHttpClient()
+              provideAnimationsAsync(), provideHttpClient(),
+              {provide: LOCALE_ID, useValue: 'es'}
   ]
 };
---
+++ src/polyfills.ts
@@ -0,0 +1,19 @@
+// src/polyfills.ts
+
+/***************************************************************************************************
+ * BROWSER POLYFILLS
+ */
+
+// Si estás usando características modernas de JavaScript, puedes necesitar incluir algunos polyfills aquí.
+
+/***************************************************************************************************
+ * Zone JS is required by Angular itself.
+ */
+import 'zone.js'; // Incluido con Angular CLI.
+
+/***************************************************************************************************
+ * IMPORTACIÓN DE LA APLICACIÓN
+ */
+
+// Soporte para internacionalización (i18n)
+import '@angular/localize/init';
